bf85302bd7f59bb8088ff647f55d6b3f14dc53e4
Sealence Shao <sealence@gmail.com>
2009-07-20 06:55:05 -0400

    another

---
+++ ace/prestashop/tools/smarty/compile/%%FE^FE9^FE93C973%%index.tpl.php
@@ -0,0 +1,3 @@
+<?php /* Smarty version 2.6.20, created on 2009-07-16 12:06:07
+         compiled from /home/sealence/local/ace/prestashop/themes/brownsugar/index.tpl */ ?>
+<?php echo $this->_tpl_vars['HOOK_HOME']; ?>
