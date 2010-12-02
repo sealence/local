0b17b82484f302a930414ad16d99d40789f74959
Sealence Shao <sealence@gmail.com>
2010-12-02 12:29:13 -0500

    Sync on office

--- ace/prestashop/tools/smarty/compile/%%FE^FE9^FE93C973%%index.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.20, created on 2009-07-16 12:06:07
-         compiled from /home/sealence/local/ace/prestashop/themes/brownsugar/index.tpl */ ?>
-<?php echo $this->_tpl_vars['HOOK_HOME']; ?>
