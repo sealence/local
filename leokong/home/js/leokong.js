e028567b52ae91b589a08b2b9b2385cf8907a821
Sealence Shao <sealence@gmail.com>
2010-12-22 12:06:07 -0500

    Add xmas

--- leokong/home/js/leokong.js
+++ leokong/home/js/leokong.js
@@ -98,7 +98,50 @@ $(function() {
       }
     });
 
+    // Initialize Advanced Galleriffic Gallery
+    var gallery2 = $('#thumbs2').galleriffic({
+      delay:                     2500,
+      numThumbs:                 9,
+      preloadAhead:              10,
+      enableTopPager:            true,
+      enableBottomPager:         true,
+      maxPagesToShow:            7,
+      imageContainerSel:         '#slideshow2',
+      controlsContainerSel:      '#controls2',
+      captionContainerSel:       '#caption2',
+      loadingContainerSel:       '#loading2',
+      renderSSControls:          true,
+      renderNavControls:         true,
+      playLinkText:              'Play Slideshow',
+      pauseLinkText:             'Pause Slideshow',
+      prevLinkText:              '&lsaquo; Prev',
+      nextLinkText:              'Next &rsaquo;',
+      nextPageLinkText:          'Next &rsaquo;',
+      prevPageLinkText:          '&lsaquo; Prev',
+      enableHistory:             false,
+      autoStart:                 false,
+      syncTransitions:           true,
+      defaultTransitionDuration: 900,
+      onSlideChange:             function(prevIndex, nextIndex) {
+        // 'this' refers to the gallery, which is an extension of $('#thumbs')
+        this.find('ul.thumbs').children()
+          .eq(prevIndex).fadeTo('fast', onMouseOutOpacity).end()
+          .eq(nextIndex).fadeTo('fast', 1.0);
+      },
+      onPageTransitionOut:       function(callback) {
+        this.fadeTo('fast', 0.0, callback);
+      },
+      onPageTransitionIn:        function() {
+        this.fadeTo('fast', 1.0);
+      }
+    });
+
     $container.hide();
+
+    $('#xmas').click(function(){
+      $container.hide();
+      $gc0.show();
+    });
 
     $('#w2010show').click(function(){
       $container.hide();
@@ -107,7 +150,7 @@ $(function() {
 
     $('#m2010show').click(function(){
       $container.hide();
-      $gc0.show();
+      $gc2.show();
     });
 
 /*
