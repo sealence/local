151af8ec8d3c10cd04ee5475006d39a3a749d4e5
Sealence Shao <sealence@gmail.com>
2011-01-05 06:50:02 -0500

    Autumn and winter

--- leokong/home/js/leokong.js
+++ leokong/home/js/leokong.js
@@ -5,6 +5,7 @@ $(function() {
     var $gc0 = $('#gc0');
     var $gc1 = $('#gc1');
     var $gc2 = $('#gc2');
+    var $gc3 = $('#gc3');
 
     //font replace
     Cufon.replace('.fb,h1');
@@ -140,6 +141,44 @@ $(function() {
       }
     });
 
+    // Initialize Advanced Galleriffic Gallery
+    var gallery3 = $('#thumbs3').galleriffic({
+      delay:                     2500,
+      numThumbs:                 9,
+      preloadAhead:              0,
+      enableTopPager:            true,
+      enableBottomPager:         true,
+      maxPagesToShow:            7,
+      imageContainerSel:         '#slideshow3',
+      controlsContainerSel:      '#controls3',
+      captionContainerSel:       '#caption3',
+      loadingContainerSel:       '#loading3',
+      renderSSControls:          true,
+      renderNavControls:         true,
+      playLinkText:              'Play Slideshow',
+      pauseLinkText:             'Pause Slideshow',
+      prevLinkText:              '&lsaquo; Prev',
+      nextLinkText:              'Next &rsaquo;',
+      nextPageLinkText:          'Next &rsaquo;',
+      prevPageLinkText:          '&lsaquo; Prev',
+      enableHistory:             false,
+      autoStart:                 false,
+      syncTransitions:           true,
+      defaultTransitionDuration: 900,
+      onSlideChange:             function(prevIndex, nextIndex) {
+        // 'this' refers to the gallery, which is an extension of $('#thumbs')
+        this.find('ul.thumbs').children()
+          .eq(prevIndex).fadeTo('fast', onMouseOutOpacity).end()
+          .eq(nextIndex).fadeTo('fast', 1.0);
+      },
+      onPageTransitionOut:       function(callback) {
+        this.fadeTo('fast', 0.0, callback);
+      },
+      onPageTransitionIn:        function() {
+        this.fadeTo('fast', 1.0);
+      }
+    });
+
     //the Dock
     var $dock = $('.dock');
     //jqDock
@@ -151,9 +190,14 @@ $(function() {
 
     $container.hide();
 
+    $('#wm2010aw').click(function(){
+      $container.hide();
+      $gc3.show();
+    });
+
     $('#xmas').click(function(){
       $container.hide();
-      $gc0.show();
+      $gc2.show();
     });
 
     $('#w2010ss').click(function(){
@@ -163,7 +207,7 @@ $(function() {
 
     $('#m2010ss').click(function(){
       $container.hide();
-      $gc2.show();
+      $gc0.show();
     });
 
     $('#follow').click(function(){
